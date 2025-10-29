1293ba8b338d7746a2e091dc93c8a39df8579567
Felippe Alves <flipgm@gmail.com>
2025-10-29 20:22:31 -0300

    Overall plot

--- analysis/plots.py
+++ analysis/plots.py
@@ -202,7 +202,7 @@ def _(FOUNDATIONS_ORDER, RESULTS_DIR, alt, df, pl):
         _p_data
         # .transform_filter(alt.datum.foundat)
         .mark_bar(size=15).encode(
-            alt.X("susceptibility:Q",title="Susceptibility"),
+            alt.X("susceptibility:Q", title="Susceptibility"),
             alt.Y("foundation:N", title=None, sort=FOUNDATIONS_ORDER),
             alt.Color("model:N", title=""),
             opacity=alt.when(alt.datum.foundation == "All Foundations")
@@ -212,7 +212,7 @@ def _(FOUNDATIONS_ORDER, RESULTS_DIR, alt, df, pl):
     )
 
     _p_error = _p_data.mark_errorbar().encode(
-        alt.X("susceptibility:Q",title="Susceptibility"),
+        alt.X("susceptibility:Q", title="Susceptibility"),
         alt.XError("susceptibility_uncertainty:Q"),
         alt.Y("foundation:N", title=None, sort=FOUNDATIONS_ORDER),
     )
@@ -227,7 +227,7 @@ def _(FOUNDATIONS_ORDER, RESULTS_DIR, alt, df, pl):
         color="#000000",
     ).encode(
         alt.Y("foundation:N"),
-        alt.X("susceptibility:Q",title="Susceptibility"),
+        alt.X("susceptibility:Q", title="Susceptibility"),
         alt.Text("s_label:N"),
         color=alt.value("black"),
         opacity=alt.value(1.0),
@@ -267,7 +267,7 @@ def _(FOUNDATIONS_ORDER, RESULTS_DIR, alt, df, pl):
         _p_data
         # .transform_filter(alt.datum.foundat)
         .mark_bar(size=15).encode(
-            alt.X("s_zscore:Q",title="Susceptibility Z-Score"),
+            alt.X("s_zscore:Q", title="Susceptibility Z-Score"),
             alt.Y("foundation:N", title=None, sort=FOUNDATIONS_ORDER),
             alt.Color("model:N", title=""),
             opacity=alt.when(alt.datum.foundation == "All Foundations")
@@ -277,7 +277,7 @@ def _(FOUNDATIONS_ORDER, RESULTS_DIR, alt, df, pl):
     )
 
     _p_error = _p_data.mark_errorbar().encode(
-        alt.X("s_zscore:Q",title="Susceptibility Z-Score"),
+        alt.X("s_zscore:Q", title="Susceptibility Z-Score"),
         alt.XError("s_zscore_error:Q"),
         alt.Y("foundation:N", title=None, sort=FOUNDATIONS_ORDER),
     )
@@ -292,7 +292,7 @@ def _(FOUNDATIONS_ORDER, RESULTS_DIR, alt, df, pl):
         color="#000000",
     ).encode(
         alt.Y("foundation:N"),
-        alt.X("s_zscore:Q",title="Susceptibility Z-Score"),
+        alt.X("s_zscore:Q", title="Susceptibility Z-Score"),
         alt.Text("sz_label:N"),
         color=alt.value("black"),
         opacity=alt.value(1.0),
@@ -332,7 +332,7 @@ def _(FOUNDATIONS_ORDER, RESULTS_DIR, alt, df, pl):
         _p_data
         # .transform_filter(alt.datum.foundat)
         .mark_bar(size=15).encode(
-            alt.X("robustness:Q",title="Robustness"),
+            alt.X("robustness:Q", title="Robustness"),
             alt.Y("foundation:N", title=None, sort=FOUNDATIONS_ORDER),
             alt.Color("model:N", title=""),
             opacity=alt.when(alt.datum.foundation == "All Foundations")
@@ -342,7 +342,7 @@ def _(FOUNDATIONS_ORDER, RESULTS_DIR, alt, df, pl):
     )
 
     _p_error = _p_data.mark_errorbar().encode(
-        alt.X("robustness:Q",title="Robustness"),
+        alt.X("robustness:Q", title="Robustness"),
         alt.XError("robustness_uncertainty:Q"),
         alt.Y("foundation:N", title=None, sort=FOUNDATIONS_ORDER),
     )
@@ -357,7 +357,7 @@ def _(FOUNDATIONS_ORDER, RESULTS_DIR, alt, df, pl):
         color="#000000",
     ).encode(
         alt.Y("foundation:N"),
-        alt.X("robustness:Q",title="Robustness"),
+        alt.X("robustness:Q", title="Robustness"),
         alt.Text("r_label:N"),
         color=alt.value("black"),
         opacity=alt.value(1.0),
@@ -397,7 +397,7 @@ def _(FOUNDATIONS_ORDER, RESULTS_DIR, alt, df, pl):
         _p_data
         # .transform_filter(alt.datum.foundat)
         .mark_bar(size=15).encode(
-            alt.X("r_zscore:Q",title="Robustness Z-Score"),
+            alt.X("r_zscore:Q", title="Robustness Z-Score"),
             alt.Y("foundation:N", title=None, sort=FOUNDATIONS_ORDER),
             alt.Color("model:N", title=""),
             opacity=alt.when(alt.datum.foundation == "All Foundations")
@@ -407,7 +407,7 @@ def _(FOUNDATIONS_ORDER, RESULTS_DIR, alt, df, pl):
     )
 
     _p_error = _p_data.mark_errorbar().encode(
-        alt.X("r_zscore:Q",title="Robustness Z-Score"),
+        alt.X("r_zscore:Q", title="Robustness Z-Score"),
         alt.XError("r_zscore_error:Q"),
         alt.Y("foundation:N", title=None, sort=FOUNDATIONS_ORDER),
     )
@@ -422,7 +422,7 @@ def _(FOUNDATIONS_ORDER, RESULTS_DIR, alt, df, pl):
         color="#000000",
     ).encode(
         alt.Y("foundation:N"),
-        alt.X("r_zscore:Q",title="Robustness Z-Score"),
+        alt.X("r_zscore:Q", title="Robustness Z-Score"),
         alt.Text("rz_label:N"),
         color=alt.value("black"),
         opacity=alt.value(1.0),
@@ -446,414 +446,63 @@ def _(FOUNDATIONS_ORDER, RESULTS_DIR, alt, df, pl):
     return
 
 
-app._unparsable_cell(
-    r"""
-    Create a scatter plot for @table[\"moral_metrics\"] with column model in the Y-axis, column \"susceptibility\" in the X-axis, column \"susceptibility_uncertainty\" as the error in the X-Axis, with column \"s_label\" as annotations, removing the grid the right and top spines, using the altair library
-    """,
-    name="_"
-)
-
-
 @app.cell
-def _(alt):
-    def model_scatter(
-        df,
-        metric,
-        metric_uncertainty,
-        metric_label,
-        *,
-        metric_title=None,
-        dx=0,
-        dy=-5,
-        size=50,
-        height=150,
-        width=200,
-    ):
-        scatter_data = alt.Chart(df)
+def _(RESULTS_DIR, alt, table):
+    _p_data = alt.Chart(table["moral_metrics"], height=200, width=200)
 
-        y_axis = alt.Y(
-            "model:N",
-            title="Model",
-            sort=alt.SortField(metric, order="descending"),
-        )
-        x_axis = alt.X(
-            metric + ":Q",
-            title=(metric_title if metric_title else metric.capitalize()),
-        )
-
-        points = scatter_data.mark_point(
-            color="black", filled=True, size=size, opacity=1.0
-        ).encode(y_axis, x_axis)
-
-        error_bars = scatter_data.mark_errorbar().encode(
-            y_axis, x_axis, alt.XError(metric_uncertainty + ":Q")
-        )
-
-        labels = scatter_data.mark_text(
-            align="center", baseline="bottom", dx=dx, dy=dy, fontSize=7
-        ).encode(y_axis, x_axis, text=metric_label + ":N")
-
-        scatter_plot = (
-            (points + error_bars + labels)
-            .configure_axis(grid=False)
-            .configure_view(strokeWidth=0)
-            .properties(
-                title="Susceptibility by Model with Uncertainty",
-                width=width,
-                height=height,
-            )
-        )
-
-        return scatter_plot
-    return (model_scatter,)
-
-
-@app.cell
-def _(model_scatter, table):
-    model_scatter(
-        table["moral_metrics"],
-        "susceptibility",
-        "susceptibility_uncertainty",
-        "s_label",
-        size=20,
-    )
-    return
-
-
-@app.cell
-def _(model_scatter, table):
-    model_scatter(
-        table["moral_metrics"],
-        "robustness",
-        "robustness_uncertainty",
-        "r_label",
-        dx=15,
-        size=20,
-    )
-    return
-
-
-@app.cell
-def _(alt, table):
-    _foundation_data = alt.Chart(table["moral_metrics_per_foundation"])
-
-    _y_axis = alt.Y(
-        "model:N",
-        title="Model",
-        sort=alt.SortField("susceptibility", order="descending"),
-    )
-    _x_axis = alt.X("susceptibility:Q", title="Susceptibility")
-
-    _points = _foundation_data.mark_point(
-        color="black", filled=True, size=50, opacity=1.0
-    ).encode(
-        _y_axis, _x_axis, alt.Color("foundation:N"), alt.Column("foundation:N")
+    _p_bars_s = _p_data.mark_bar(size=15, opacity=0.6).encode(
+        alt.Y("model:N"), alt.X("susceptibility:Q"), alt.Color("model:N")
     )
 
-    _error_bars = _foundation_data.mark_errorbar().encode(
-        _y_axis, _x_axis, alt.XError("susceptibility_uncertainty:Q")
+    _p_error_s = _p_data.mark_errorbar().encode(
+        alt.Y("model:N"),
+        alt.X("susceptibility:Q"),
+        alt.XError("susceptibility_uncertainty:Q"),
     )
 
-    _labels = _foundation_data.mark_text(
-        align="center", baseline="bottom", dx=0, dy=-5, fontSize=8
-    ).encode(_y_axis, _x_axis, text="s_label:N")
+    _p_label_s = _p_bars_s.mark_text(
+        align="right",
+        baseline="middle",
+        dx=-15,
+        dy=0,
+        fontSize=9,
+        opacity=1.0,
+        color="#000000",
+    ).encode(alt.Text("s_label"), color=alt.value("black"), opacity=alt.value(1.0))
 
-    _foundation_plot = (
-        (_points + _error_bars + _labels)
-        # .facet(facet="foundation:N", columns=3)
-        .configure_axis(grid=False)
-        .configure_view(strokeWidth=0)
-        .properties(
-            title="Susceptibility by Model and Foundation",
-            # width=200,
-            # height=150,
-        )
+    _p_bars_r = _p_data.mark_bar(size=15, opacity=0.6).encode(
+        alt.Y("model:N", title=None), alt.X("robustness:Q"), alt.Color("model:N")
     )
 
-    _foundation_plot
-    return
-
-
-@app.cell
-def _():
-    # mm_data = alt.Chart(table["moral_metrics"])
-
-    # y_model = alt.Y(
-    #     "model:N",
-    #     title="Model",
-    #     sort=alt.SortField("susceptibility", order="descending"),
-    # )
-    # x_susc = alt.X("susceptibility:Q", title="Susceptibility")
-
-    # s_point_plot = (
-    #     mm_data.mark_point(color="black", filled=True, size=36)
-    #     .encode(y_model, x_susc)
-    #     .properties(title="Moral Susceptibility by Model", width=250, height=200)
-    # )
-
-    # s_error_bars = mm_data.mark_errorbar().encode(
-    #     y_model, x_susc, alt.XError("susceptibility_uncertainty")
-    # )
-
-    # s_label = s_point_plot.mark_text(
-    #     align="center", baseline="bottom", size=8, dy=-5, dx=0
-    # ).encode(text="s_label")
-
-    # s_plot = (
-    #     (s_point_plot + s_error_bars + s_label)
-    #     .configure_axis(grid=False)
-    #     .configure_view(
-    #         strokeWidth=0  # Removes the chart border
-    #     )
-    # )
-
-    # # s_plot.save(RESULTS_DIR / "susceptibility_point_plot.pdf")
-    # # s_plot.save(RESULTS_DIR / "susceptibility_point_plot.png")
-    # s_plot
-    return
-
-
-@app.cell
-def _():
-    # x_robus = alt.X("robustness:Q", title="Robustness")
-    # y_model2 = alt.Y(
-    #     "model:N",
-    #     title="Model",
-    #     sort=alt.SortField("robustness", order="descending"),
-    # )
-
-    # r_point_plot = (
-    #     mm_data.mark_point(color="black", filled=True, size=30)
-    #     .encode(y_model2, x_robus, tooltip=["model", "robustness"])
-    #     .properties(title="Moral Robustness by Model", width=250, height=300)
-    # )
-
-    # r_error_bars = mm_data.mark_errorbar().encode(
-    #     y_model2, x_robus, alt.XError("r_uncertainty")
-    # )
-
-    # r_label = r_point_plot.mark_text(
-    #     align="center", baseline="bottom", size=8, dy=-5, dx=20
-    # ).encode(text="r_and_e_label")
-
-    # r_plot = (
-    #     (r_point_plot + r_error_bars + r_label)
-    #     .configure_axis(grid=False)
-    #     .configure_view(strokeWidth=0)
-    # )
-
-    # r_plot.save(RESULTS_DIR / "robustness_point_plot.pdf")
-    # r_plot.save(RESULTS_DIR / "robustness_point_plot.png")
-    # r_plot
-    return
-
-
-@app.cell
-def _(RESULTS_DIR, alt, create_val_err_label, table, zscore, zscore_error):
-    mm_z_data = alt.Chart(
-        table["moral_metrics"]
-        .with_columns(
-            s_zscore=zscore("susceptibility"),
-            s_zscore_error=zscore_error("susceptibility", "s_uncertainty"),
-            r_zscore=zscore("robustness"),
-            r_zscore_error=zscore_error("robustness", "r_uncertainty"),
-        )
-        .with_columns(
-            sz_label=create_val_err_label("s_zscore", "s_zscore_error"),
-            rz_label=create_val_err_label("r_zscore", "r_zscore_error"),
-        )
+    _p_error_r = _p_data.mark_errorbar().encode(
+        alt.Y("model:N"),
+        alt.X("robustness:Q"),
+        alt.XError("robustness_uncertainty:Q"),
     )
 
-    sz_point_plot = (
-        mm_z_data.mark_point(color="black", filled=True)
-        .encode(
-            alt.X("s_zscore:Q", title="Susceptibility Z-Score"),
-            alt.Y(
-                "model:N",
-                title="Model",
-                sort=alt.SortField("s_zscore", order="descending"),
-            ),
+    _p_label_r = _p_bars_s.mark_text(
+        align="right",
+        baseline="middle",
+        dx=100,
+        dy=0,
+        fontSize=9,
+        opacity=1.0,
+        color="#000000",
+    ).encode(alt.Text("r_label"), color=alt.value("black"), opacity=alt.value(1.0))
+
+    _p = (
+        (
+            (_p_bars_s + _p_error_s + _p_label_s)
+            | (_p_bars_r + _p_error_r + _p_label_r)
         )
-        .properties(
-            title="Moral Susceptibility Z-Score by Model", width=250, height=300
-        )
-    )
-
-    sz_error_plot = mm_z_data.mark_errorbar().encode(
-        alt.Y(
-            "model:N",
-            title="Model",
-            sort=alt.SortField("s_zscore", order="descending"),
-        ),
-        alt.X("s_zscore:Q", title="Susceptibility Z-Score"),
-        alt.XError("s_zscore_error:Q"),
-    )
-
-    sz_label = sz_point_plot.mark_text(
-        align="center", baseline="bottom", size=8, dy=-5, dx=0
-    ).encode(text="sz_label")
-
-    sz_plot = (
-        (sz_point_plot + sz_error_plot + sz_label)
         .configure_axis(grid=False)
         .configure_view(strokeWidth=0)
     )
-    sz_plot.save(RESULTS_DIR / "susceptibility_zscore_point_plot.pdf")
-    sz_plot.save(RESULTS_DIR / "susceptibility_zscore_point_plot.png")
 
-    sz_plot
-    return mm_z_data, sz_point_plot
+    _p.save(RESULTS_DIR / "moral_metrics_overall_bars.png")
+    _p.save(RESULTS_DIR / "moral_metrics_overall_bars.pdf")
 
-
-@app.cell
-def _(RESULTS_DIR, alt, mm_z_data):
-    rz_point_plot = (
-        mm_z_data.mark_point(color="black", filled=True)
-        .encode(
-            alt.X("r_zscore:Q", title="Robustness Z-Score"),
-            alt.Y(
-                "model:N",
-                title="Model",
-                sort=alt.SortField("r_zscore", order="descending"),
-            ),
-        )
-        .properties(
-            title="Moral Robustness Z-Score by Model", width=250, height=300
-        )
-    )
-
-    rz_error_plot = mm_z_data.mark_errorbar().encode(
-        alt.Y(
-            "model:N",
-            title="Model",
-            sort=alt.SortField("r_zscore", order="descending"),
-        ),
-        alt.X("r_zscore:Q", title="Robustness Z-Score"),
-        alt.XError("r_zscore_error:Q"),
-    )
-
-    rz_label = rz_point_plot.mark_text(
-        align="center", baseline="bottom", size=8, dy=-5, dx=0
-    ).encode(text="rz_label")
-
-    rz_plot = (
-        (rz_point_plot + rz_error_plot + rz_label)
-        .configure_axis(grid=False)
-        .configure_view(strokeWidth=0)
-    )
-    rz_plot.save(RESULTS_DIR / "robustness_zscore_point_plot.pdf")
-    rz_plot.save(RESULTS_DIR / "robustness_zscore_point_plot.png")
-
-    rz_plot
-    return
-
-
-@app.cell
-def _(alt, create_val_err_label, sz_point_plot, table, zscore, zscore_error):
-    mmbf_data = alt.Chart(
-        table["moral_metrics_by_foundation"]
-        .with_columns(
-            s_zscore=zscore("susceptibility").over("foundation"),
-            s_zscore_error=zscore_error("susceptibility", "s_uncertainty").over(
-                "foundation"
-            ),
-            r_zscore=zscore("robustness").over("foundation"),
-            r_zscore_error=zscore_error("robustness", "r_uncertainty").over(
-                "foundation"
-            ),
-        )
-        .with_columns(
-            s_label=create_val_err_label("susceptibility", "s_uncertainty"),
-            sz_label=create_val_err_label("s_zscore", "s_zscore_error"),
-            r_label=create_val_err_label("robustness", "r_uncertainty"),
-            rz_label=create_val_err_label("r_zscore", "r_zscore_error"),
-        )
-    )
-
-    sbf_point_plot = (
-        mmbf_data.mark_point(color="black", filled=True)
-        .encode(
-            alt.X("susceptibility:Q", title="Susceptibility by Foundation"),
-            alt.Y(
-                "model:N",
-                title="Model",
-                sort=alt.SortField("s_zscore", order="descending"),
-            ),
-            alt.Color("foundation:N"),
-        )
-        .properties(
-            title="Moral Susceptibility Z-Score by Model", width=250, height=300
-        )
-    )
-
-    # sbf_error_plot = mmbf_data.mark_errorbar().encode(
-    #     alt.Y(
-    #         "model:N",
-    #         title="Model",
-    #         sort=alt.SortField("s_zscore", order="descending"),
-    #     ),
-    #     alt.X("s_zscore:Q", title="Susceptibility Z-Score"),
-    #     alt.XError("s_zscore_error:Q"),
-    # )
-
-    # sz_label = sz_point_plot.mark_text(
-    #     align="center", baseline="bottom", size=8, dy=-5, dx=0
-    # ).encode(text="sz_label")
-
-    sbf_plot = (
-        (sz_point_plot)  # + sz_error_plot + sz_label)
-        .configure_axis(grid=False)
-        .configure_view(strokeWidth=0)
-    )
-    # sz_plot.save(RESULTS_DIR / "susceptibility_zscore_point_plot.pdf")
-    # sz_plot.save(RESULTS_DIR / "susceptibility_zscore_point_plot.png")
-
-    sbf_plot
-    return
-
-
-@app.cell
-def _(alt, create_val_err_label, table, zscore, zscore_error):
-    (
-        alt.Chart(
-            table["moral_metrics_per_foundation"]
-            .with_columns(
-                s_zscore=zscore("susceptibility").over("foundation"),
-                s_zscore_error=zscore_error(
-                    "susceptibility", "susceptibility_uncertainty"
-                ).over("foundation"),
-                r_zscore=zscore("robustness").over("foundation"),
-                r_zscore_error=zscore_error(
-                    "robustness", "robustness_uncertainty"
-                ).over("foundation"),
-            )
-            .with_columns(
-                s_label=create_val_err_label(
-                    "susceptibility", "susceptibility_uncertainty"
-                ),
-                sz_label=create_val_err_label("s_zscore", "s_zscore_error"),
-                r_label=create_val_err_label(
-                    "robustness", "robustness_uncertainty"
-                ),
-                rz_label=create_val_err_label("r_zscore", "r_zscore_error"),
-            )
-        )
-        .mark_circle(color="black", filled=True)
-        .encode(
-            alt.X("susceptibility:Q", title=""),
-            alt.Y(
-                "model:N",
-                title="",
-                sort=alt.SortField("susceptibility", order="descending"),
-            ),
-            alt.Color("foundation:N"),
-            # alt.Column("foundation:N")
-        )
-        .facet(facet="foundation:N", columns=3)
-        .properties(
-            title="Moral Susceptibility Z-Score by Model", width=100, height=75
-        )
-    )
+    _p
     return
 
 
